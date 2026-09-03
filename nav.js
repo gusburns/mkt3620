e4279819271e0829711017cb120fcca4210af3a5
gusburns <gusburns@iastate.edu>
2026-09-03 13:39:10 -0500

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,15 @@
+// navigation.js
+
+function loadNavigation() {
+  const nav = document.createElement("nav");
+  nav.innerHTML = `
+    <ul class="nav-list">
+      <li><a href="index.html">About Me</a></li>
+      <li><a href="resume.html">Resume</a></li>
+      <li><a href="experience.html">Experience</a></li>
+      <li><a href="personal.html">Personal</a></li>
+    </ul>
+  `;
+  document.body.prepend(nav);
+}
+document.addEventListener("DOMContentLoaded", loadNavigation);
